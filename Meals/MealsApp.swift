c570e99c4930f69b1e7acc298e3b29d32419302f
Shane Brelesky <shanebrelesky@gmail.com>
2024-07-30 22:55:36 -0700

    Initial Commit

---
+++ Meals/MealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsApp.swift
+//  Meals
+//
+//  Created by Shane Brelesky on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct MealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
